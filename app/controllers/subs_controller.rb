17e6b9dac899c71e069b40b7c28451ada67fbe3b
parkerprecisionweb <dallasjamesparker@gmail.com>
2018-09-04 10:41:12 -0600

    updating routes/controller

--- app/controllers/subs_controller.rb
+++ app/controllers/subs_controller.rb
@@ -1,8 +1,13 @@
 class SubsController < ApplicationController
   def index
+    @subs = Sub.all
+    #render :view_page
+    #render :index (This is done by default, but can be overriden with syntax "render :view_page")
   end
 
   def show
+    @sub = Sub.find(params[:id])
+    #render :show
   end
 
   def new
